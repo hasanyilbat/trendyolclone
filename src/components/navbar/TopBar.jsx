70378034f02509f4cb258225aac65af51b1ac298
Hasan <hasan.yilbat@gmail.com>
2022-08-18 17:59:59 +0300

    starter

---
+++ src/components/navbar/TopBar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const topBar = () => {
+  return <div className="bg-sky-500 mx-auto">topBar</div>;
+};
+
+export default topBar;
---
+++ src/pages/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import TopBar from "../components/navbar/TopBar";
+const Main = () => {
+  return (
+    <div>
+      <TopBar />
+    </div>
+  );
+};
+
+export default Main;
